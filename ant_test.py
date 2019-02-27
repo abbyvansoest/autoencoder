4e9aefe9a78dc4fc7de7332932cdf5ab09229480
Abby Van Soest <abby@wintermute.c.brain-princeton.internal>
2019-02-27 10:47:04 -0500

    update experience collection

--- ant_test.py
+++ ant_test.py
@@ -2,37 +2,12 @@ import gym
 import numpy as np 
 from custom import CustomAutoencoder
 
-steps = 10000000
-test_steps = 5000
-reduce_dim = 10
-
 env = gym.make("Ant-v2")
-data = []
-test_data = []
 
-# collect training data
-print("collecting training data")
-obs = env.reset()
-print(len(obs))
-for i in range(steps):
-    data.append(obs[:29])
-    action = env.action_space.sample()
-    obs,r,d,_ = env.step(action)
-    
-    if i % 1000 == 0:
-        print(i)
-    
-# collect test data
-print("collecting testing data")
-obs = env.reset()
-for i in range(test_steps):
-    test_data.append(obs[:29])
-    action = env.action_space.sample()
-    obs,r,d,_ = env.step(action)
-    
-    if i % 1000 == 0:
-        print(i)
-    
+print("loading data")
+data = list(np.load("data/ant_data.npy"))
+test_data = list(np.load("data/ant_test_data.npy"))
+
 autoencoder = CustomAutoencoder(len(data[0]), 
                                 num_hid1=20,
                                 num_hid2=12,
@@ -40,7 +15,7 @@ autoencoder = CustomAutoencoder(len(data[0]),
                                 normalize=False)
 autoencoder.set_data(data)
 autoencoder.set_test_data(test_data)
-autoencoder.train(testiter=20)
+autoencoder.train(testiter=2)
 
 
 
--- collect_experience.py
+++ collect_experience.py
@@ -15,11 +15,13 @@ ant_test_data = []
 # collect training data
 print("collecting Ant training data")
 obs = env.reset()
-print(len(obs))
-for i in range(steps):
-    ant_data.append(obs[:29])
-    action = env.action_space.sample()
-    obs,r,d,_ = env.step(action)
+for i in range(int(steps/100)):
+    for j in range(int(steps/(steps/100))):
+        ant_data.append(obs[:29])
+        action = env.action_space.sample()
+        obs,r,d,_ = env.step(action)
+    
+    obs = env.reset()
     
     if i % 1000 == 0:
         print(i)
@@ -27,16 +29,21 @@ for i in range(steps):
 # collect test data
 print("collecting Ant testing data")
 obs = env.reset()
-for i in range(test_steps):
-    ant_test_data.append(obs[:29])
-    action = env.action_space.sample()
-    obs,r,d,_ = env.step(action)
+for i in range(int(test_steps/100)):
+    for j in range(int(test_steps/(test_steps/100))):
+        ant_test_data.append(obs[:29])
+        action = env.action_space.sample()
+        obs,r,d,_ = env.step(action)
+        
+    obs = env.reset()
     
     if i % 1000 == 0:
         print(i)
 
-np.save("data/ant_data", ant_data)
-np.save("data/ant_test_data", ant_test_data)
+np.save("data/ant_data_reset", ant_data)
+np.save("data/ant_test_data_reset", ant_test_data)
+
+#######################################
 
 env = gym.make("Humanoid-v2")
 humanoid_data = []
@@ -45,11 +52,13 @@ humanoid_test_data = []
 # collect training data
 print("collecting Humanoid training data")
 obs = env.reset()
-print(len(obs))
-for i in range(steps):
-    humanoid_data.append(obs[:269])
-    action = env.action_space.sample()
-    obs,r,d,_ = env.step(action)
+for i in range(int(steps/100)):
+    for j in range(int(steps/(steps/100))):
+        humanoid_data.append(obs[:269])
+        action = env.action_space.sample()
+        obs,r,d,_ = env.step(action)
+    
+    obs = env.reset()
     
     if i % 1000 == 0:
         print(i)
@@ -57,13 +66,16 @@ for i in range(steps):
 # collect test data
 print("collecting Humanoid testing data")
 obs = env.reset()
-for i in range(test_steps):
-    humanoid_test_data.append(obs[:269])
-    action = env.action_space.sample()
-    obs,r,d,_ = env.step(action)
+for i in range(int(test_steps/100)):
+    for j in range(int(test_steps/(test_steps/100))):
+        humanoid_test_data.append(obs[:269])
+        action = env.action_space.sample()
+        obs,r,d,_ = env.step(action)
+        
+    obs = env.reset()
     
     if i % 1000 == 0:
         print(i)
 
-np.save("data/humanoid_data", humanoid_data)
-np.save("data/humanoid_test_data", humanoid_test_data)
+np.save("data/humanoid_data_reset", humanoid_data)
+np.save("data/humanoid_test_data_reset", humanoid_test_data)
